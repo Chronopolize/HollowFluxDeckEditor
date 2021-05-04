57cdb4f026eebd0ee32bde35a3afeb39c8daa79f
Colin Ta <colinta@Live.ca>
2021-05-04 01:54:53 -0700

    Defined overall UI. Active panel displays hovered card.

--- cards.js
+++ cards.js
@@ -1,6 +1,3 @@
-
-
-
 $( document ).ready(function() {
 	console.log("hi")
   addCards();
@@ -8,10 +5,27 @@ $( document ).ready(function() {
 
 function addCards(){
  
- let dest = $('#cards').first();
+ let dest = $('.cardListingPanel').first();
  //150 cards in total
- for (i=1;i<=60;i++){
- 	let img = $(`<img class="card-image" src="./img/cards/${i}.jpg"></img>`)
+ for (i=1;i<=20;i++){
+ 	let img = $(`<img class="card-image" cardid="${i}" src="./img/cards/${i}.jpg"></img>`)
+
+let onHover = function (){
+	
+	let cardid = $(this).attr("cardid");
+	console.log("hovered cardid", cardid)
+
+
+	let panel = $('.selectedCard')
+	panel.attr("src", `./img/cards/${cardid}.jpg`);
+	panel.show();
+	
+}
+let offHover = function(){
+	$('.selectedCard').hide();
+}
+ 	
+ 	img.hover(onHover, offHover);
  	dest.append(img);
  }
  
